d2e443b975c168f3ccfd48ac2c1011b39ea36b4e
vitawell <41141374+vitawell@users.noreply.github.com>
2022-06-24 11:48:57 +0800

    Add files via upload

---
+++ data/noise.py
@@ -0,0 +1,88 @@
+import os
+import numpy as np
+import random
+import cv2
+
+def sp_noise(noise_img, proportion):
+    '''
+    添加椒盐噪声
+    proportion的值表示加入噪声的量，可根据需要自行调整
+    return: noise_img
+    '''
+    height, width = noise_img.shape[0], noise_img.shape[1]#获取高度宽度像素值
+    num = int(height * width * proportion) #一个准备加入多少噪声小点
+    for i in range(num):
+        w = random.randint(0, width - 1)
+        h = random.randint(0, height - 1)
+        if random.randint(0, 1) == 0:
+            noise_img[h, w] = 0
+        else:
+            noise_img[h, w] = 255
+    return noise_img
+
+
+def gaussian_noise(img, mean, sigma):
+    '''
+    此函数用将产生的高斯噪声加到图片上
+    传入:
+        img   :  原图
+        mean  :  均值
+        sigma :  标准差
+    返回:
+        gaussian_out : 噪声处理后的图片
+    '''
+    # 将图片灰度标准化
+    img = img / 255
+    # 产生高斯 noise
+    noise = np.random.normal(mean, sigma, img.shape)
+    # 将噪声和图片叠加
+    gaussian_out = img + noise
+    # 将超过 1 的置 1，低于 0 的置 0
+    gaussian_out = np.clip(gaussian_out, 0, 1)
+    # 将图片灰度范围的恢复为 0-255
+    gaussian_out = np.uint8(gaussian_out*255)
+    
+    return gaussian_out# 这里也会返回噪声，注意返回值
+
+
+def random_noise(image,noise_num):
+    '''
+    添加随机噪点（实际上就是随机在图像上将像素点的灰度值变为255即白色）
+    image: 需要加噪的图片
+    noise_num: 添加的噪音点数目
+    return: img_noise
+    '''
+    # 参数image：，noise_num：
+    img_noise = image
+    # cv2.imshow("src", img)
+    rows, cols, chn = img_noise.shape
+    # 加噪声
+    for i in range(noise_num):
+        x = np.random.randint(0, rows)#随机生成指定范围的整数
+        y = np.random.randint(0, cols)
+        img_noise[x, y, :] = 255
+    return img_noise
+
+
+def convert(input_dir, output_dir):
+    for filename in os.listdir(input_dir):
+        path = input_dir + "/" + filename # 获取文件路径
+        
+        fname,ext = os.path.splitext(filename)
+        if ext == '.jpg':
+            print("doing... ", path)
+            img = cv2.imread(path)#读取图片
+            img_noise = gaussian_noise(img, 0, 0.12) # 高斯噪声
+            img_noise = sp_noise(img_noise,0.025)# 椒盐噪声
+            img_noise  = random_noise(img_noise,500)# 随机噪声
+            cv2.imwrite(output_dir+'/'+filename,img_noise )
+
+if __name__ == '__main__':
+    input_dir = "/data1/multispectral-object-detection/data/seacutest/rgb"    # 输入数据文件夹
+    output_dir = "/data1/multispectral-object-detection/data/seacutest/rgb" # 输出数据文件夹
+    os.makedirs(output_dir,exist_ok=True) 
+    convert(input_dir, output_dir)
+
+
+
+
