9669ef86e9328688978803716d90ddd47edba556
vitawell <41141374+vitawell@users.noreply.github.com>
2022-07-15 18:58:47 +0800

    Update imageex.py

--- data/imageex.py
+++ data/imageex.py
@@ -1,16 +1,13 @@
 import os
 from PIL import Image
 path = "/data1/multispectral-object-detection/data/tctest/depth"
-
 path2 = "/data1/multispectral-object-detection/data/tctest/images" 
 
 def is_image_file(filename):
     return any(filename.endswith(extension) for extension in ['.png', '.jpg', '.jpeg', '.PNG', '.JPG', '.JPEG'])
 
-
 #files= os.listdir(path) #得到文件夹下的所有图片
 image_filenames = [os.path.join(path, x) for x in os.listdir(path) if is_image_file(x)]
-
 
 for img_filename in image_filenames:
     filename = os.path.basename(img_filename)
@@ -26,8 +23,7 @@ for img_filename in image_filenames:
     w2 = img2.width       #图片的宽
     h2 = img2.height      #图片的高
     
-    if w < w2 or h <h2:  # 如果深度图小于原图
-        
+    if w < w2 or h <h2:  # 如果深度图小于原图，则改变深度图大小
         img = img.resize((w2, h2))
         img.save(img_filename)
         print(img_filename)
