ebf68b864f635c945d22bf51a52c0d270a930689
vitawell <41141374+vitawell@users.noreply.github.com>
2022-07-21 15:31:48 +0800

    Update dataclean.py

--- data/dataclean.py
+++ data/dataclean.py
@@ -1,15 +1,18 @@
 import os
 path = "/data1/multispectral-object-detection/data/DUO/depth/train"
-#train and test
+#train and test 先将txr文件复制到文件夹中
 path2 = "/data1/multispectral-object-detection/data/DUO/images/train" 
 
 #files= os.listdir(path) #得到文件夹下的所有文件名称
 #s = []
 for i in range(1,6672):
-    img_path = os.path.join(path, str(i)) + '.jpg'
-    txt_path = os.path.join(path, str(i)) + '.txt'
-    img_path2 = os.path.join(path2, str(i)) + '.jpg'
-    txt_path2 = os.path.join(path2, str(i)) + '.txt'
+    n=str(i)
+    n=n.zfill(6) #前面用0填充到6位
+    
+    img_path = os.path.join(path, n) + '.jpg'
+    txt_path = os.path.join(path, n) + '.txt'
+    img_path2 = os.path.join(path2, n) + '.jpg'
+    txt_path2 = os.path.join(path2, n) + '.txt'
     if not os.path.exists(img_path):  # 如果不能打开图片
         print(img_path)
         if os.path.exists(txt_path):  # 如果txt存在，则删除
