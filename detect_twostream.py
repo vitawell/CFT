380984bb6b1c71623833d3f3c83ae814bd55c512
vitawell <41141374+vitawell@users.noreply.github.com>
2022-12-10 10:32:31 +0800

    Update detect_twostream.py

--- detect_twostream.py
+++ detect_twostream.py
@@ -82,18 +82,106 @@ def detect(opt):
         t1 = time_synchronized()
         #pred = model(img, img2, augment=opt.augment)[0]
         ##pred为输出的推理out
-        pred = model(img, img2, augment=opt.augment)[1]
+        pred = model(img, img2, augment=opt.augment)[0]
         out = []
         for k in range(0,len(pred)):
             out.append(pred[k][0])
-        for k in range(1,len(pred)):
-            out[0]=torch.cat((out[0],out[k]),1) 
-        pred = out[0] #将三个detect结果concat
+        #for k in range(1,len(pred)):
+            #out[0]=torch.cat((out[0],out[k]),1) 
+        #pred = out[0] #将三个detect结果concat
+        
+        pred1 = out[0]  #depth
+        pred2 = out[1]  #rgb
 
         # Apply NMS
-        pred = non_max_suppression(pred, opt.conf_thres, opt.iou_thres, classes=opt.classes, agnostic=opt.agnostic_nms)
+        #pred = non_max_suppression(pred, opt.conf_thres, opt.iou_thres, classes=opt.classes, agnostic=opt.agnostic_nms)
+        pred1 = non_max_suppression(pred1, opt.conf_thres, opt.iou_thres, classes=opt.classes, agnostic=opt.agnostic_nms)
+        pred2 = non_max_suppression(pred2, opt.conf_thres, opt.iou_thres, classes=opt.classes, agnostic=opt.agnostic_nms)
+        
         t2 = time_synchronized()
 
+        ###
+        # Process detections
+        for i, det in enumerate(pred):  # detections per image
+
+            if webcam:  # batch_size >= 1
+                p, s, im0, frame = path[i], '%g: ' % i, im0s[i].copy(), dataset.count
+            else:
+                p, s, im0, frame = path, '', im0s.copy(), getattr(dataset, 'frame', 0)
+                p, s, im0_, frame = path, '', im0s_.copy(), getattr(dataset2, 'frame', 0)
+
+            p = Path(p)  # to Path
+            save_path = str(save_dir / p.name)  # img.jpg
+            txt_path = str(save_dir / 'labels' / p.stem) + ('' if dataset.mode == 'image' else f'_{frame}')  # img.txt
+            s += '%gx%g ' % img.shape[2:]  # print string
+            gn = torch.tensor(im0.shape)[[1, 0, 1, 0]]  # normalization gain whwh
+            # print(gn)
+
+            # print(det)
+            if len(det):
+                # Rescale boxes from img_size to im0 size
+                det[:, :4] = scale_coords(img.shape[2:], det[:, :4], im0.shape).round()
+
+                # Print results
+                for c in det[:, -1].unique():
+                    n = (det[:, -1] == c).sum()  # detections per class
+                    s += f"{n} {names[int(c)]}{'s' * (n > 1)}, "  # add to string
+
+                # Write results
+                # xyxy是预测框左上角和右下角坐标
+                for *xyxy, conf, cls in reversed(det):
+                    if save_txt:  # Write to file
+                        xywh = (xyxy2xywh(torch.tensor(xyxy).view(1, 4)) / gn).view(-1).tolist()  # normalized xywh
+                        line = (cls, *xywh, conf) if opt.save_conf else (cls, *xywh)  # label format
+                        with open(txt_path + '.txt', 'a') as f:
+                            f.write(('%g ' * len(line)).rstrip() % line + '\n')
+
+                    if save_img or opt.save_crop or view_img:  # Add bbox to image
+                        c = int(cls)  # integer class
+                        label = None if opt.hide_labels else (names[c] if opt.hide_conf else f'{names[c]} {conf:.2f}')
+                        
+                        # rgb图像是im0
+                        plot_one_box(xyxy, im0, label=label, color=colors(c, True), line_thickness=opt.line_thickness)
+                        # ir图像是im0_
+                        plot_one_box(xyxy, im0_, label=label, color=colors(c, True), line_thickness=opt.line_thickness)
+                        if opt.save_crop:
+                            save_one_box(xyxy, im0s, file=save_dir / 'crops' / names[c] / f'{p.stem}.jpg', BGR=True)
+
+            # Print time (inference + NMS)
+            print(f'{s}Done. ({t2 - t1:.6f}s, {1/(t2 - t1):.6f}Hz)')
+            # add all the fps
+            img_num += 1
+            fps_sum += 1/(t2 - t1)
+
+            # Stream results
+            if view_img:
+                cv2.imshow(str(p), im0)
+                cv2.waitKey(1)  # 1 millisecond
+
+            # Save results (image with detections)
+            if save_img:
+                if dataset.mode == 'image':
+                    save_path_rgb = save_path.split('.')[0] + '_rgb.' + save_path.split('.')[1]
+                    save_path_ir = save_path.split('.')[0] + '_ir.' + save_path.split('.')[1]
+                    print(save_path_rgb)
+                    cv2.imwrite(save_path_rgb, im0)
+                    cv2.imwrite(save_path_ir, im0_)
+                else:  # 'video' or 'stream'
+                    if vid_path != save_path:  # new video
+                        vid_path = save_path
+                        if isinstance(vid_writer, cv2.VideoWriter):
+                            vid_writer.release()  # release previous video writer
+                        if vid_cap:  # video
+                            fps = vid_cap.get(cv2.CAP_PROP_FPS)
+                            w = int(vid_cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+                            h = int(vid_cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+                        else:  # stream
+                            fps, w, h = 30, im0.shape[1], im0.shape[0]
+                            save_path += '.mp4'
+                        vid_writer = cv2.VideoWriter(save_path, cv2.VideoWriter_fourcc(*'mp4v'), fps, (w, h))
+                    vid_writer.write(im0)
+        
+        ###
         # Apply Classifier
         if classify:
             pred = apply_classifier(pred, modelc, img, im0s)
