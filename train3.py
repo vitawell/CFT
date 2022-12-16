b0a0c71a3ced7f8ff4a2e6e70182653bbc0870e3
vitawell <41141374+vitawell@users.noreply.github.com>
2022-12-16 16:32:02 +0800

    Update train3.py

--- train3.py
+++ train3.py
@@ -375,20 +375,31 @@ def train_rgb_ir(hyp, opt, device, tb_writer=None):
                 #loss, loss_items = compute_loss(pred, targets.to(device))  # loss scaled by batch_size
                 
                 ### 分别计算loss
-                pred1 = pred[0]
-                pred2 = pred[1]
-                #pred3 = pred[2]
+                if len(pred)==3:
+                    pred1 = pred[0]
+                    pred2 = pred[1]
+                    pred3 = pred[2]
+                    
+                    loss1, loss_items1 = compute_loss(pred1, targets.to(device))
+                    loss2, loss_items2 = compute_loss(pred2, targets.to(device))
+                    loss3, loss_items3 = compute_loss(pred3, targets.to(device))
+                    
+                    a,b,c = 0.5, 0.5, 0.5  #loss权重
+                    loss = loss1 * a + loss2 * b + loss3 * c
+                    loss_items = loss_items1* a + loss_items2 * b + loss_items3 * c
+                    
+                elif len(pred)==2:
+                    pred1 = pred[0]
+                    pred2 = pred[1]
                 
-                loss1, loss_items1 = compute_loss(pred1, targets.to(device))
-                loss2, loss_items2 = compute_loss(pred2, targets.to(device))
-                #loss3, loss_items3 = compute_loss(pred3, targets.to(device))
                 
-                a,b = 1, 1
-                loss = loss1 * a + loss2 * b
-                #loss_items = loss_items1* a + loss_items2 * b
-                #a,b,c = 0.5, 0.5, 0.5  #loss权重
-                #loss = loss1 * a + loss2 * b + loss3 * c
-                #loss_items = loss_items1* a + loss_items2 * b + loss_items3 * c
+                    loss1, loss_items1 = compute_loss(pred1, targets.to(device))
+                    loss2, loss_items2 = compute_loss(pred2, targets.to(device))
+                
+                
+                    a,b = 0.5, 0.5
+                    loss = loss1 * a + loss2 * b
+                    loss_items = loss_items1* a + loss_items2 * b
                 
                 ###
                 if rank != -1:
